039be487de081eef6f308c66e73fc703159534cd
Aman Yadav <ay091339@gmail.com>
2024-02-15 09:52:44 +0100

    following one

---
+++ src/screens/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+function Cart() {
+  return (
+    <View>
+    <Text>Cart</Text>
+    </View>
+  )
+}
+
+export default Cart
---
+++ src/screens/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+
+function Welcome() {
+    return (
+        <View>
+            <Text>Hello from Welcome!!</Text>
+        </View>
+    )
+}
+
+export default Welcome
